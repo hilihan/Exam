a78203abc8fd4b33c0c1feae6efa88939ada3086
hilihan <hiliguoxin@foxmail.com>
2017-02-19 12:21:53 +0800

    项目环境搭建
    创建项目，引入Hibernate和Structs环境，创建Hibernate工厂

---
+++ src/cn/mark/util/HibernateUtil.java
@@ -0,0 +1,22 @@
+package cn.mark.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    
+    private static SessionFactory buildSessionFactory(){
+        Configuration configuration = new Configuration().configure();
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+    
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+    
+}
